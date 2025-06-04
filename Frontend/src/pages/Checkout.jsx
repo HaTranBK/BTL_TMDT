0791a0eb8c5becb93160f67d0e5c477c1263e1f7
Han <han.lehoangngoc@hcmut.edu.vn>
2025-06-05 00:52:45 +0700

    done Checkout

--- Frontend/src/pages/Checkout.jsx
+++ Frontend/src/pages/Checkout.jsx
@@ -5,7 +5,7 @@ import CheckoutDetails from '../components/Checkout/CheckoutDetails';
 import OrderComplete from '../components/Checkout/OrderComplete';
 
 const Checkout = () => {
-    // localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjksInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTc0OTA0Nzg0MiwiZXhwIjoxNzQ5MTM0MjQyfQ.J2CFsVv2XB2WinPowLcsby10YSrhoI7nAJtLQ1gB_iQ');
+    // localStorage.setItem('token', '');
 
     const [step, setStep] = useState('shopping-cart');
 
